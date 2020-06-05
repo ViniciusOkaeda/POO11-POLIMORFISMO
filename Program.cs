26a0076ede8cf6176b06d6d499a9bf1f0203b6ad
Vinicius <viniciusokaeda@gmail.com>
2020-06-05 14:34:49 -0300

    Estrutura poo polimorfismo overload finalizada

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace AULA11POLIMORFISMO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Calculo infoPlayer = new Calculo();
+           // 1° opção sem argumentos
+           System.Console.WriteLine( infoPlayer.Calcular() );
+
+
+           // 2° opção com 1 argumento int
+           System.Console.WriteLine(infoPlayer.Calcular(100) );
+
+            
+            //3° opção com 2 argumentos int
+            System.Console.WriteLine(infoPlayer.Calcular(100,40) );
+
+
+            //4° opção com 2 argumentos string
+            System.Console.WriteLine(infoPlayer.Calcular("Bruce", "Banner"));
         }
     }
 }
